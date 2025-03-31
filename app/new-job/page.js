cff52d75373ac3ee804dfa2e9ba4740bc366b8a8
carlos trujillo <trujillo.r.c.06@gmail.com>
2025-03-31 03:22:35 -0700

    Fixed css styling on new job form page

--- app/new-job/page.js
+++ app/new-job/page.js
@@ -4,9 +4,7 @@ import styles from "../../components/jobs/new-job-form.module.css";
 export default async function NewJobPage() {
   return (
     <main>
-      <section className={styles.filler}></section>
       <NewJobForm />
-      <section className={styles.filler}></section>
     </main>
   );
 }
