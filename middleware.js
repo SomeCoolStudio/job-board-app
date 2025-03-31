4681d31712852288460af518d583ffd6d4c4afd5
carlos trujillo <trujillo.r.c.06@gmail.com>
2025-03-30 21:13:01 -0700

    Fixed middleware.js not being tracked

---
+++ middleware.js
@@ -0,0 +1,8 @@
+import { withMiddlewareAuthRequired } from "@auth0/nextjs-auth0/edge";
+
+export default withMiddlewareAuthRequired();
+
+// Apply to the /new-job route:
+export const config = {
+  matcher: ["/new-job"],
+};
