842d9fcafcf742069eb1ee2e6c14a11c240bc397
carlos trujillo <trujillo.r.c.06@gmail.com>
2025-04-01 12:46:42 -0700

    Removed middleware.js for testing auth0 bug

--- middleware.js
+++
@@ -1,8 +0,0 @@
-import { withMiddlewareAuthRequired } from "@auth0/nextjs-auth0/edge";
-
-export default withMiddlewareAuthRequired();
-
-// Apply to the /new-job route:
-export const config = {
-  matcher: ["/new-job"],
-};
