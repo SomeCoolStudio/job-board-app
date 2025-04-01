e1f2ac1b8690045a620962770f1ce75dd6633fc5
carlos trujillo <trujillo.r.c.06@gmail.com>
2025-04-01 16:05:12 -0700

    Enhancing middleware.js to bypass authentication for certain requests

--- middleware.js
+++ middleware.js
@@ -1,8 +1,20 @@
 import { withMiddlewareAuthRequired } from "@auth0/nextjs-auth0/edge";
+import { NextResponse } from "next/server";
 
-export default withMiddlewareAuthRequired();
+export default withMiddlewareAuthRequired((req) => {
+  // Bypass preflight requests or RSC fetches
+  if (
+    req.method === "OPTIONS" ||
+    req.nextUrl.searchParams.has("_rsc") ||
+    req.headers.get("access-control-request-method") ||
+    (req.headers.get("Accept") &&
+      req.headers.get("Accept").includes("text/x-component"))
+  ) {
+    return NextResponse.next();
+  }
+  // Otherwise, proceed with the default authentication logic.
+});
 
-// Apply to the /new-job route:
 export const config = {
   matcher: ["/new-job"],
 };
