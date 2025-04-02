a15289a219bc5d3739c5afeaaaa4738b34f77a3b
carlos trujillo <trujillo.r.c.06@gmail.com>
2025-04-01 18:06:32 -0700

    Adding middleware.js for better user exp

--- app/new-job/page.js
+++ app/new-job/page.js
@@ -1,13 +1,9 @@
 import NewJobForm from "@/components/jobs/new-job-form";
-import styles from "../../components/jobs/new-job-form.module.css";
-import ProtectedRoute from "@/components/nav/protected-route";
 
 export default async function NewJobPage() {
   return (
-    <ProtectedRoute>
-      <main>
-        <NewJobForm />
-      </main>
-    </ProtectedRoute>
+    <main>
+      <NewJobForm />
+    </main>
   );
 }
