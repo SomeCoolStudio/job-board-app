c190c979e5e762a608934e51238ba6b2a16fbeda
carlos trujillo <trujillo.r.c.06@gmail.com>
2025-04-01 17:31:33 -0700

    Fixed useUser import in protected-route.js

--- components/nav/protected-route.js
+++ components/nav/protected-route.js
@@ -1,7 +1,7 @@
 "use client";
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
-import { useUser } from "@auth0/nextjs-auth0";
+import { useUser } from "@auth0/nextjs-auth0/client";
 
 export default function ProtectedRoute({ children }) {
   const { user, isLoading } = useUser();
